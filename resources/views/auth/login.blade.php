172797ca05502c78a447e755dc22740b43a52b22
Eman <amirm3607@gmail.com>
2024-06-14 06:31:23 -0700

    removee

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,6 +6,10 @@
         @csrf
 
         <!-- Email Address -->
+        <div>
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        </div>
 
         <!-- Password -->
         <div class="mt-4">
